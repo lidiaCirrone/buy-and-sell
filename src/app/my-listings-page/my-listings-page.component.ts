4b37fa94b7790e133dd4980ac0b3193c44c43c1d
Lidia Cirrone <l.cirrone.esterno@almaviva.it>
2024-01-25 16:48:18 +0100

    my listings page

--- src/app/my-listings-page/my-listings-page.component.ts
+++ src/app/my-listings-page/my-listings-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeMyListings } from '../fake-data';
 
 @Component({
   selector: 'app-my-listings-page',
   templateUrl: './my-listings-page.component.html',
-  styleUrl: './my-listings-page.component.scss'
+  styleUrl: './my-listings-page.component.scss',
 })
-export class MyListingsPageComponent {
+export class MyListingsPageComponent implements OnInit {
+  listings?: Listing[];
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.listings = fakeMyListings;
+  }
+
+  onDeleteClicked(listingId: string): void {
+    alert(`Deleting your listing with id ${listingId}`);
+  }
 }
