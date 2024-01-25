5db347d0e12c871a551057ea6802b3d9b755aa20
Lidia Cirrone <l.cirrone.esterno@almaviva.it>
2024-01-25 15:14:41 +0100

    listings page + fake data

--- src/app/listings-page/listings-page.component.ts
+++ src/app/listings-page/listings-page.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-listings-page',
   templateUrl: './listings-page.component.html',
-  styleUrl: './listings-page.component.scss'
+  styleUrl: './listings-page.component.scss',
 })
-export class ListingsPageComponent {
+export class ListingsPageComponent implements OnInit {
+  listings: Listing[] = [];
 
+  constructor() {}
+  
+  ngOnInit(): void {
+    this.listings = fakeListings;
+  }
 }
