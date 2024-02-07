380058e1e73bf3609eded9241afbca28bfa52262
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-02-07 17:56:36 +0100

    added a updateListing endpoint

--- buy-and-sell-backend/src/routes/index.js
+++ buy-and-sell-backend/src/routes/index.js
@@ -3,11 +3,13 @@ import { createNewListingRoute } from "./createNewListing";
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListing";
 import { getUserListingsRoute } from "./getUserListings";
+import { updateListingRoute } from "./updateListing";
 
 export default [
    addViewToListingRoute,
    createNewListingRoute,
    getAllListingsRoute,
    getListingRoute,
-   getUserListingsRoute
+   getUserListingsRoute,
+   updateListingRoute
 ]
