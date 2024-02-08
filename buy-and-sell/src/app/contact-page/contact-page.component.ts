27b12e1a4fa992b0c76b42ae47114699fd246a46
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-02-08 15:06:31 +0100

    use the ListingsService getListingById() method inside the contact component

--- buy-and-sell/src/app/contact-page/contact-page.component.ts
+++ buy-and-sell/src/app/contact-page/contact-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Listing } from '../types';
 import { ActivatedRoute, Router } from '@angular/router';
-import { fakeListings } from '../fake-data';
+import { ListingsService } from '../listings.service';
 
 @Component({
   selector: 'app-contact-page',
@@ -13,12 +13,18 @@ export class ContactPageComponent implements OnInit {
   message: string = '';
   listing?: Listing;
 
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private listingsService: ListingsService
+  ) {}
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.listing = fakeListings.find((listing) => listing.id === id);
-    this.message = `Hi, I'm interested in your ${this.listing?.name.toLowerCase()}!`;
+    const id = this.route.snapshot.paramMap.get('id') ?? '';
+    this.listingsService.getListingById(id).subscribe((listing) => {
+      this.listing = listing;
+      this.message = `Hi, I'm interested in your ${this.listing?.name.toLowerCase()}!`;
+    });
   }
 
   sendMessage(): void {
