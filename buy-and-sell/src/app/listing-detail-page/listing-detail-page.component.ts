c7d1aaa43abc5aa105b530c2e55f9e28ad957504
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-02-08 15:04:44 +0100

    use the ListingsService getListingById() method

--- buy-and-sell/src/app/listing-detail-page/listing-detail-page.component.ts
+++ buy-and-sell/src/app/listing-detail-page/listing-detail-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Listing } from '../types';
-import { fakeListings } from '../fake-data';
+import { ListingsService } from '../listings.service';
 
 @Component({
   selector: 'app-listing-detail-page',
@@ -9,12 +9,22 @@ import { fakeListings } from '../fake-data';
   styleUrl: './listing-detail-page.component.scss',
 })
 export class ListingDetailPageComponent implements OnInit {
+  isLoading: boolean = true;
   listing?: Listing;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private listingsService: ListingsService
+  ) {}
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.listing = fakeListings.find((listing) => listing.id === id);
+    const id = this.route.snapshot.paramMap.get('id') ?? '';
+    this.listingsService.getListingById(id).subscribe((listing) => {
+      this.listing = listing;
+      this.isLoading = false;
+    });
+    this.listingsService
+      .addViewToListing(id)
+      .subscribe(() => console.log('Views updated!'));
   }
 }
