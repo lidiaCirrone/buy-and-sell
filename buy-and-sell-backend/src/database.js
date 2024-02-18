4933292623c45bff8b38631ff835e2f38af0a418
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-02-18 17:15:49 +0100

    vercel deployment, test 9 (installed mysql2)

--- buy-and-sell-backend/src/database.js
+++ buy-and-sell-backend/src/database.js
@@ -1,20 +1,10 @@
-import mysql from 'mysql';
+const mysql = require('mysql2');
 
-const fs = require('fs');
 let connection;
 
 export const db = {
    connect: () => {
-      connection = mysql.createConnection({
-         host: process.env.DB_HOST,
-         user: process.env.DB_USER,
-         password: process.env.DB_PASS,
-         database: process.env.DB_NAME,
-         ssl: {
-            ca: process.env.DB_SSL_CA,
-            // ca : fs.readFileSync(__dirname + process.env.DB_SSL_CA)
-         }
-      })
+      connection = mysql.createConnection(process.env.DB_URL)
       connection.connect();
    },
    query: (queryString, escapedValues) =>
