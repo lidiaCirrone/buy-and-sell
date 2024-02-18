5fc9e73f9864c13b006625c26c945fc6cdedbffd
Lidia Cirrone <lidia.cirrone@gmail.com>
2024-02-18 17:05:55 +0100

    vercel deployment, test 7

--- buy-and-sell-backend/src/database.js
+++ buy-and-sell-backend/src/database.js
@@ -1,10 +1,20 @@
 import mysql from 'mysql';
 
+const fs = require('fs');
 let connection;
 
 export const db = {
    connect: () => {
-      connection = mysql.createConnection(process.env.DB_URL)
+      connection = mysql.createConnection({
+         host: process.env.DB_HOST,
+         user: process.env.DB_USER,
+         password: process.env.DB_PASS,
+         database: process.env.DB_NAME,
+         ssl: {
+            // ca: process.env.DB_SSL_CA,
+            ca : fs.readFileSync(__dirname + process.env.DB_SSL_CA)
+         }
+      })
       connection.connect();
    },
    query: (queryString, escapedValues) =>
